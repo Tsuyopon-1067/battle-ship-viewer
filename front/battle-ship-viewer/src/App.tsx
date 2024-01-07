de78c9aad36d4af3f1196356d4b54321aa2013d4
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-01-07 20:05:50 +0900

    :sparkles: switch auto reload, slider history selection

--- front/battle-ship-viewer/src/App.tsx
+++ front/battle-ship-viewer/src/App.tsx
@@ -1,30 +1,58 @@
 import './App.css';
 import BattleTable from './BattleTable';
 import { TableDataType, initialBattleTableData } from './type';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import HistorySlider from './HistorySlider';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
 function App() {
-  const [battleTableData, setBattleTableData] = useState<TableDataType>(initialBattleTableData);
+  //const battleTableDataHistory:TableDataType[] = ([initialBattleTableData]);
   const [battleTableDataHistory, setbattleTableDataHistory] = useState<TableDataType[]>([initialBattleTableData]);
-  const longPolling = async () => {
-      try {
-        const response = await fetch("http://localhost:50000/getjson/");
-        const jsonData = await response.json();
-        setBattleTableData(jsonData);
-      } catch (error) {
-        console.error('ロングポーリングエラー:', error);
-      } finally {
-        setTimeout(longPolling, 2000);
-      }
-    };
+  const selectedHisotyrIndexRef = useRef<number>(0);
+  const [selectedHisotyrIndex, setSelectedHisotyrIndex] = useState<number>(0);
+  const isEnableAutoUpdate = useRef<boolean>(false);
+  const maxValueRef = useRef<number>(0);
+  const [maxValue, setMaxValue] = useState<number>(0);
+  const [sliderValue, setSliderValue] = useState<number>(0);
+  const updateSliderValue = (value: number) => {
+    selectedHisotyrIndexRef.current = value;
+    setSelectedHisotyrIndex(value);
+    setSliderValue(value);
+  }
+  const changeSwitch = (event: React.ChangeEvent) => {
+    const checked = (event.target as HTMLInputElement).checked;
+    isEnableAutoUpdate.current = checked;
+  }
 
+  useEffect(() => {
+    const longPolling = () => {
+      fetch("http://localhost:50000/getjson/")
+        .then((response) => response.json())
+        .then((data) => {
+          battleTableDataHistory.push(data);
+          maxValueRef.current = battleTableDataHistory.length - 1;
+          setMaxValue(maxValueRef.current);
+          if (isEnableAutoUpdate.current) {
+            selectedHisotyrIndexRef.current = maxValueRef.current;
+            setSelectedHisotyrIndex(maxValueRef.current);
+            updateSliderValue(maxValueRef.current);
+          }
+        })
+      .catch((error) => console.error("Error:", error))
+      .finally(() => {
+        longPolling();
+      });
+    };
     longPolling();
+  }, []);
 
   return (
-
-
     <div className="App">
-      <BattleTable data={battleTableData} />
+      {selectedHisotyrIndex}/{maxValue}
+      <BattleTable data={battleTableDataHistory[selectedHisotyrIndexRef.current]} />
+      <HistorySlider maxValue={maxValueRef.current} value={sliderValue} updateValue={updateSliderValue} />
+      <FormControlLabel control={<Switch onChange={changeSwitch} />} label="Auto Update" />
     </div>
   );
 }
